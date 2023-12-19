77c417d0360fd5cb83715a45280db279297a6195
Aman Chauhan <amanch2209@gmail.com>
2023-12-19 22:50:04 +0530

    frontend deployed testing

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3002
 
 app.use(cors(
   {
-    origin : [],
+    origin : ["https://yog-guru-frontend-api.vercel.app"],
     methods : ["POST"],
     credentials : true
   }
